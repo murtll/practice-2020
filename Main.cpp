79bcb91db9b19e7ace6f8742bebe0498771261ca
BorisVikVik <boris@victorov.com>
2020-06-22 12:48:12 +0300

    Added task 7

--- Main.cpp
+++ Main.cpp
@@ -707,6 +707,365 @@ void runTask5V3(int episode)
 	}
 }
 
+void runTask7(int episode)
+{
+	try
+	{
+		game->loadConfig(path + "\\scenarios\\task7.cfg");
+		game->setWindowVisible(true);
+		game->setRenderWeapon(true);
+		game->init();
+	}
+	catch (std::exception & e)
+	{
+		std::cout << e.what() << std::endl;
+	}
+
+	std::vector<double> actions = { 0,0,0,0 };
+
+	auto image = cv::Mat(480, 640, CV_8UC3);
+	auto greyscale = cv::Mat(480, 640, CV_8UC1);
+	//auto clusters = cv::Mat(480, 640, CV_8UC3);
+	//auto centers = cv::Mat(480, 640, CV_8UC3);
+
+	cv::Mat clusters;
+
+
+	for (auto i = 0; i < episode; i++)
+	{
+		game->newEpisode();
+		std::cout << "Episode #" << i + 1 << std::endl;
+
+		while (!game->isEpisodeFinished())
+		{
+			int center = 0;
+			int k = 0;
+			const auto& gameState = game->getState();
+			std::memcpy(image.data, gameState->screenBuffer->data(), gameState->screenBuffer->size());
+			vizdoom::BufferPtr screenBuf = gameState->screenBuffer;
+			cv::extractChannel(image, greyscale, 0);
+			cv::threshold(greyscale, greyscale,	175, 255, cv::THRESH_BINARY);
+			imshow("LOL", greyscale);
+			//game->makeAction(actions[2]);
+			//image.data = screenBuf->data();
+			//cvtColor(image, GrayImage, cv::COLOR_BGR2GRAY);
+			//line(image, Point(0, 245), Point(639, 245), Scalar(0, 0, 200));
+			std::vector<cv::Point2f> centers;
+			std::vector<cv::Point2f> points(0);
+			for (int x = 290; x < 350; x++)
+			{
+				for (int y = 0; y < 300; y++)
+				{
+					if ((int(image.at<cv::Vec3b>(y, x)[2]) > 130 && int(image.at<cv::Vec3b>(y, x)[2]) < 150 && int(image.at<cv::Vec3b>(y, x)[0]) < 50 && int(image.at<cv::Vec3b>(y, x)[0]) > 30) || (int(image.at<cv::Vec3b>(y, x)[0]) > 150 && int(image.at<cv::Vec3b>(y, x)[0]) < 200 && int(image.at<cv::Vec3b>(y, x)[1]) > 100))
+					{
+						greyscale.at<unsigned char>(y, x) = 255;
+						circle(image, cv::Point(x, y), 1, cv::Scalar(0, 255, 0), 1, 8, 0);
+						points.push_back(cv::Point2f(x, y));
+						center += x;
+						k++;
+					}
+					else
+					{
+						greyscale.at<unsigned char>(y, x) = 0;
+					}
+				}
+			}
+			/*cv::rectangle(image,))*/
+			greyscale.convertTo(greyscale, CV_32F);
+			//cv::Mat samples = greyscale.reshape(1, greyscale.total());
+			/*int count = 0;
+			for (int x = 0; x < 640; x++)
+			{
+				for (int y = 0; y < 480; y++)
+				{
+					if (int(image.at<cv::Vec3b>(y, x)[2]) > 130 && int(image.at<cv::Vec3b>(y, x)[0]) < 50)
+					{
+						points[count] = cv::Point2f(x, y);
+						count++;
+					}
+				}
+			}*/
+			if(points.size() > 1)cv::kmeans(points, 2, clusters, cv::TermCriteria(cv::TermCriteria::EPS + cv::TermCriteria::COUNT, 10, 1.0), 3, cv::KMEANS_RANDOM_CENTERS, centers);
+
+			//centers = centers.reshape(1, 0);
+			//clusters = clusters.reshape(1, greyscale.rows);
+
+			greyscale.convertTo(greyscale, CV_8UC3);
+
+			/*cvKMeans2(&greyscale, 1, &clusters, cvTermCriteria(CV_TERMCRIT_EPS + CV_TERMCRIT_ITER, 10, 1.0));
+			cvZero(&clusters);
+
+			for (i = 0; i < 5; i++) {
+				CvPoint2D32f pt = ((CvPoint2D32f*) greyscale.data)[i];
+				circle(clusters, cvPointFrom32f(pt), 2, cvScalar(0, 255, 0), CV_FILLED);
+			}*/
+
+			/*goodFeaturesToTrack(GrayImage, corners, 25, 0.01, 2);
+			//for (int i = 0; i < corners.size(); i++)
+			//{
+			//	cv::circle(image, Point(corners[i].x, corners[i].y), 10, Scalar(255, 255, 255), -1, 8, 0);
+			//	center += corners[i].y;
+			//	k++;
+			//}*/
+			/*if (k != 0)
+			{
+				center /= k;
+			}*/
+
+			//circle(image, cv::Point(center, 245), 1, cv::Scalar(0, 0, 255), -1, 10, 0);
+			//circle(image, cv::Point(center, 245), 3, cv::Scalar(0, 0, 255), -1, 8, 0);
+
+			//cv::Point2f c = centers.at<cv::Point2f>(0);
+			if (points.size() > 1) 
+			{
+				cv::Point c = centers[0];
+				cv::circle(image, c, 5, cv::Scalar(255, 0, 255), -1, 8);
+			}
+
+			/*cv::Point sdsaf = centers[1];
+			cv::circle(image, sdsaf, 5, cv::Scalar(255, 255, 255), -1, 8);*/
+			////cout << endl;
+			////waitKey(100);
+			////cout << image.at<uchar>(245, 340) << endl;
+			////cout << intensity.val[2] << endl;
+			//imshow("Clusters", clusters);
+			imshow("Game", image);
+			bool shoot = false;
+			if (points.size() > 1) 
+			{
+				for (int clust = 0; clust < centers.size(); clust++)
+				{
+					if (centers[clust].y > 240)
+					{
+						double err = centers[clust].x - 320;
+						double p = err * 0.1;
+						integral = integral + err * 0.005;
+						//double i += err * 0.01;
+						double u = p + integral;
+						actions = { 0, 0, u, 0 };
+						if (abs(centers[clust].x - 320) < 10)
+						{
+							actions = { 0,0,0,1 };
+							shoot = true;
+						}
+					}
+					else if (shoot != true)
+					{
+						//actions = { 1,0,0,0 };
+					}
+				}
+			}
+			else
+			{
+				actions = { 0,0,-10,0 };
+			}
+			//clusters.
+			game->makeAction(actions);
+			/*if (KEY_DOWN('A'))
+			{
+				game->makeAction(actions[1]);
+			}
+			else if (KEY_DOWN('D'))
+			{
+				game->makeAction(actions[0]);
+			}
+			else if (KEY_DOWN('K'))
+			{
+				game->makeAction(actions[2]);
+			}*/
+			//if (game->getEpisodeTime() < 70 || game->getEpisodeTime() > 200)game->makeAction(actions[0]);
+			//else game->makeAction(actions[1]);
+			//game->makeAction(actions[2]);
+		//cv::imshow("game", GrayImage);
+			cv::waitKey(sleepTime);
+		}
+		std::cout << game->getTotalReward() << std::endl;
+
+		//std::cout << clusters.rows << " " << clusters.cols << std::endl;
+		//std::cout << centers << std::endl;
+	}
+}
+
+void runTask7(int episode)
+{
+	try
+	{
+		game->loadConfig(path + "\\scenarios\\task7.cfg");
+		game->setWindowVisible(true);
+		game->setRenderWeapon(true);
+		game->init();
+	}
+	catch (std::exception & e)
+	{
+		std::cout << e.what() << std::endl;
+	}
+
+	std::vector<double> actions = { 0,0,0,0 };
+
+	auto image = cv::Mat(480, 640, CV_8UC3);
+	auto greyscale = cv::Mat(480, 640, CV_8UC1);
+	//auto clusters = cv::Mat(480, 640, CV_8UC3);
+	//auto centers = cv::Mat(480, 640, CV_8UC3);
+
+	cv::Mat clusters;
+
+
+	for (auto i = 0; i < episode; i++)
+	{
+		game->newEpisode();
+		std::cout << "Episode #" << i + 1 << std::endl;
+
+		while (!game->isEpisodeFinished())
+		{
+			int center = 0;
+			int k = 0;
+			const auto& gameState = game->getState();
+			std::memcpy(image.data, gameState->screenBuffer->data(), gameState->screenBuffer->size());
+			vizdoom::BufferPtr screenBuf = gameState->screenBuffer;
+			cv::extractChannel(image, greyscale, 0);
+			cv::threshold(greyscale, greyscale,	175, 255, cv::THRESH_BINARY);
+			imshow("LOL", greyscale);
+			//game->makeAction(actions[2]);
+			//image.data = screenBuf->data();
+			//cvtColor(image, GrayImage, cv::COLOR_BGR2GRAY);
+			//line(image, Point(0, 245), Point(639, 245), Scalar(0, 0, 200));
+			std::vector<cv::Point2f> centers;
+			std::vector<cv::Point2f> points(0);
+			for (int x = 290; x < 350; x++)
+			{
+				for (int y = 0; y < 300; y++)
+				{
+					if ((int(image.at<cv::Vec3b>(y, x)[2]) > 130 && int(image.at<cv::Vec3b>(y, x)[2]) < 150 && int(image.at<cv::Vec3b>(y, x)[0]) < 50 && int(image.at<cv::Vec3b>(y, x)[0]) > 30) || (int(image.at<cv::Vec3b>(y, x)[0]) > 150 && int(image.at<cv::Vec3b>(y, x)[0]) < 200 && int(image.at<cv::Vec3b>(y, x)[1]) > 100))
+					{
+						greyscale.at<unsigned char>(y, x) = 255;
+						circle(image, cv::Point(x, y), 1, cv::Scalar(0, 255, 0), 1, 8, 0);
+						points.push_back(cv::Point2f(x, y));
+						center += x;
+						k++;
+					}
+					else
+					{
+						greyscale.at<unsigned char>(y, x) = 0;
+					}
+				}
+			}
+			/*cv::rectangle(image,))*/
+			greyscale.convertTo(greyscale, CV_32F);
+			//cv::Mat samples = greyscale.reshape(1, greyscale.total());
+			/*int count = 0;
+			for (int x = 0; x < 640; x++)
+			{
+				for (int y = 0; y < 480; y++)
+				{
+					if (int(image.at<cv::Vec3b>(y, x)[2]) > 130 && int(image.at<cv::Vec3b>(y, x)[0]) < 50)
+					{
+						points[count] = cv::Point2f(x, y);
+						count++;
+					}
+				}
+			}*/
+			if(points.size() > 1)cv::kmeans(points, 2, clusters, cv::TermCriteria(cv::TermCriteria::EPS + cv::TermCriteria::COUNT, 10, 1.0), 3, cv::KMEANS_RANDOM_CENTERS, centers);
+
+			//centers = centers.reshape(1, 0);
+			//clusters = clusters.reshape(1, greyscale.rows);
+
+			greyscale.convertTo(greyscale, CV_8UC3);
+
+			/*cvKMeans2(&greyscale, 1, &clusters, cvTermCriteria(CV_TERMCRIT_EPS + CV_TERMCRIT_ITER, 10, 1.0));
+			cvZero(&clusters);
+
+			for (i = 0; i < 5; i++) {
+				CvPoint2D32f pt = ((CvPoint2D32f*) greyscale.data)[i];
+				circle(clusters, cvPointFrom32f(pt), 2, cvScalar(0, 255, 0), CV_FILLED);
+			}*/
+
+			/*goodFeaturesToTrack(GrayImage, corners, 25, 0.01, 2);
+			//for (int i = 0; i < corners.size(); i++)
+			//{
+			//	cv::circle(image, Point(corners[i].x, corners[i].y), 10, Scalar(255, 255, 255), -1, 8, 0);
+			//	center += corners[i].y;
+			//	k++;
+			//}*/
+			/*if (k != 0)
+			{
+				center /= k;
+			}*/
+
+			//circle(image, cv::Point(center, 245), 1, cv::Scalar(0, 0, 255), -1, 10, 0);
+			//circle(image, cv::Point(center, 245), 3, cv::Scalar(0, 0, 255), -1, 8, 0);
+
+			//cv::Point2f c = centers.at<cv::Point2f>(0);
+			if (points.size() > 1) 
+			{
+				cv::Point c = centers[0];
+				cv::circle(image, c, 5, cv::Scalar(255, 0, 255), -1, 8);
+			}
+
+			/*cv::Point sdsaf = centers[1];
+			cv::circle(image, sdsaf, 5, cv::Scalar(255, 255, 255), -1, 8);*/
+			////cout << endl;
+			////waitKey(100);
+			////cout << image.at<uchar>(245, 340) << endl;
+			////cout << intensity.val[2] << endl;
+			//imshow("Clusters", clusters);
+			imshow("Game", image);
+			bool shoot = false;
+			if (points.size() > 1) 
+			{
+				for (int clust = 0; clust < centers.size(); clust++)
+				{
+					if (centers[clust].y > 240)
+					{
+						double err = centers[clust].x - 320;
+						double p = err * 0.1;
+						integral = integral + err * 0.005;
+						//double i += err * 0.01;
+						double u = p + integral;
+						actions = { 0, 0, u, 0 };
+						if (abs(centers[clust].x - 320) < 10)
+						{
+							actions = { 0,0,0,1 };
+							shoot = true;
+						}
+					}
+					else if (shoot != true)
+					{
+						//actions = { 1,0,0,0 };
+					}
+				}
+			}
+			else
+			{
+				actions = { 0,0,-10,0 };
+			}
+			//clusters.
+			game->makeAction(actions);
+			/*if (KEY_DOWN('A'))
+			{
+				game->makeAction(actions[1]);
+			}
+			else if (KEY_DOWN('D'))
+			{
+				game->makeAction(actions[0]);
+			}
+			else if (KEY_DOWN('K'))
+			{
+				game->makeAction(actions[2]);
+			}*/
+			//if (game->getEpisodeTime() < 70 || game->getEpisodeTime() > 200)game->makeAction(actions[0]);
+			//else game->makeAction(actions[1]);
+			//game->makeAction(actions[2]);
+		//cv::imshow("game", GrayImage);
+			cv::waitKey(sleepTime);
+		}
+		std::cout << game->getTotalReward() << std::endl;
+
+		//std::cout << clusters.rows << " " << clusters.cols << std::endl;
+		//std::cout << centers << std::endl;
+	}
+}
+
+
 int main()
 {
 	try
